651fe8a2b366217dd90253fa011a3f62c4358f77
doctor305 <94644276@qq.com>
2018-10-19 17:19:31 +0800

    update

--- search_and_copy.py
+++ search_and_copy.py
@@ -9,3 +9,25 @@ Created on 2018年10月19日
 
 import os
 
+def search_file(path,extension):
+    list_files = []
+    for root,dirs,files in os.walk(path):
+        for name in files:
+            if name[-len(extension):]==extension:
+                list_files.append(root+os.sep+name)
+    return list_files
+
+def copy_file(s_file,t_file):
+    with open(s_file,'r') as f_r:
+        with open(t_file,'w') as f_w:
+            f_w.write(f_r.read())
+
+            
+            
+if __name__ == "__main__":
+    filelist = search_file('sdir','.txt')
+    print(filelist)
+    for file in filelist:
+        copy_file(file,'tdir'+os.sep+file.split(os.sep)[-1])
+        with open(file,'r') as f:
+            print(f.read())
