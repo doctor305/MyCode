319a644ae0c84529db95fff6298a67717de86083
doctor305 <94644276@qq.com>
2018-11-06 23:50:03 +0800

    update

--- zippw.py
+++ zippw.py
@@ -12,25 +12,24 @@ from threading import Thread
 import time
 
 
-def unzip(zipfile,password):
-    try:
-        zfile.extractall(pwd=password.encode('utf8'))
-    except:
-        return False
-    return password
-
 def unzip_by_dic(zipfile,pswd_list):
     global tag
+    print("Start!")
+    print(len(pswd_list))
+    print(tag)
     for pswd in pswd_list:
         if tag == True:
+            print("End!")
             break
         try:
-            zfile.extractall(pwd=password.encode('utf8'))
+            zipfile.extractall(pwd=pswd.encode('utf8'))
+            print('Password is %s' % pswd)
+            tag = True
+            print("End!")
+            break
         except:
             continue
-        print(pswd)
-        tag = True
-        break
+        
     
 def get_pwlist():
     pwlist = []
@@ -42,14 +41,23 @@ def get_pwlist():
 
 def main():
     global tag
-    path = 'test2.zip'
-    num_thread = 10
+    path = 'test.zip'
+    num_thread = 5
     tag = False
+    time_start = time.time()
     pwlist = get_pwlist()
+    print(len(pwlist))
     zfile = zipfile.ZipFile(path)
     step = len(pwlist)//num_thread+1
     for n in range(num_thread):
-        t = Thread(target = unzip_by_dic,args = (zfile,pwlist[step*n:step*n+step]))
+        t = Thread(target = unzip_by_dic,args = (zfile,pwlist[step*n:step*n+step],))
+        t.start()
+    t.join()
+    print(tag)
+    print('End')
+    time_end = time.time()
+    time_use = time_end - time_start
+    print(u'用时 %d时%d分%d秒' % (time_use//3600,time_use%3600//60,time_use%60))
         
 
 if __name__ == '__main__':
