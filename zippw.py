822b192fd3ff41e81d53910a313ebf6528950136
doctor305 <94644276@qq.com>
2018-11-06 23:24:06 +0800

    update

--- zippw.py
+++ zippw.py
@@ -8,24 +8,49 @@ Created on 2018年10月31日
 
 
 import zipfile
+from threading import Thread
+import time
 
 
-
-def unzip(path,password):
-    zfile = zipfile.ZipFile(path)
+def unzip(zipfile,password):
     try:
         zfile.extractall(pwd=password.encode('utf8'))
     except:
         return False
     return password
 
-def unzip_by_dic(path):
+def unzip_by_dic(zipfile,pswd_list):
+    global tag
+    for pswd in pswd_list:
+        if tag == True:
+            break
+        try:
+            zfile.extractall(pwd=password.encode('utf8'))
+        except:
+            continue
+        print(pswd)
+        tag = True
+        break
+    
+def get_pwlist():
+    pwlist = []
     with open('passwddic.txt','r') as f:
         for line in f.readlines():
-            password = unzip(path,line.strip('\n'))
-            if password != False:
-                print(password)
-                break
+            password = line.strip('\n')
+            pwlist.append(password)
+    return pwlist
+
+def main():
+    global tag
+    path = 'test2.zip'
+    num_thread = 10
+    tag = False
+    pwlist = get_pwlist()
+    zfile = zipfile.ZipFile(path)
+    step = len(pwlist)//num_thread+1
+    for n in range(num_thread):
+        t = Thread(target = unzip_by_dic,args = (zfile,pwlist[step*n:step*n+step]))
+        
 
 if __name__ == '__main__':
-    unzip_by_dic('test.zip')
+    main()
