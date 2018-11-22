01b7cc628bc045bf10e21a5f29f8bd3512cf2b6e
doctor305 <94644276@qq.com>
2018-11-22 23:24:59 +0800

    update

--- mssql_cracker.py
+++ mssql_cracker.py
@@ -49,28 +49,7 @@ def get_pwlist():
             pwlist.append(password)
     return pwlist
 
-def main_cracker(win):
-    global tag
-    tag = False
-    server = '127.0.0.1'
-    num_thread = 10
-    time_start = time.time()
-    pwlist = get_pwlist()
-    step = len(pwlist)//num_thread+1  
-    for n in range(num_thread):
-        t1 = Thread(target = cracker,args = (server,pwlist[step*n:step*n+step],str(n+1),))
-        t1.start()
-    t1.join()
-    
-    if not tag:
-        print(u'密码字典测试完毕，未找到匹配密码，将遍历测试8位以内所有数字密码！')
-        for m in range(10):
-            t2 = Thread(target = cracker,args = (server,range(m*10000000,(m+1)*10000000),str(m+1)+'+',))
-            t2.start()
-        t2.join()
-    time_end = time.time()
-    time_use = time_end - time_start
-    print(u'用时 %d时%d分%d秒' % (time_use//3600,time_use%3600//60,time_use%60))
+
     
 class CRACKER(object):
     def __init__(self):
@@ -79,19 +58,19 @@ class CRACKER(object):
         windows.resizable(False, False)
         windows.title("MSSQL密码破解 ")
         label = ttk.Label(windows,text=u'服务器地址：')
-        label.grid(row=0,column=0, padx=6,pady=10, sticky=tk.W)
-        vartext = tk.StringVar()
-        entry = tk.Entry(windows,width=25,textvariable=vartext)
-        entry.grid(row=0,column=1,padx=6,pady=10, sticky=tk.W)
-        button = tk.Button(windows,width=6,text="测试密码",command=select_excel)
-        button.grid(row=0,column=2,padx=6,pady=8,rowspan=2, sticky=tk.W)
+        label.grid(row=0,column=0, padx=6,pady=5,sticky=tk.W)
+        vartext_ip = tk.StringVar()
+        entry = tk.Entry(windows,width=15,textvariable=vartext_ip)
+        entry.grid(row=0,column=1,padx=6,pady=5, sticky=tk.W)
+        button = tk.Button(windows,width=6,text="测试密码",command=self.main_cracker)
+        button.grid(row=0,column=2,rowspan=2, sticky=tk.W)
         label = ttk.Label(windows,text=u'账号：')
-        label.grid(row=1,column=0, padx=6,pady=10, sticky=tk.W)
-        vartext = tk.StringVar()
-        entry = tk.Entry(windows,width=25,textvariable=vartext)
-        entry.grid(row=1,column=1,padx=6,pady=10, sticky=tk.W)
+        label.grid(row=1,column=0, padx=6,pady=5,sticky=tk.W)
+        vartext_user = tk.StringVar()
+        entry = tk.Entry(windows,width=15,textvariable=vartext_user)
+        entry.grid(row=1,column=1,padx=6,pady=5, sticky=tk.W)
         scr = scrolledtext.ScrolledText(windows, width=30, height=10, wrap=tk.WORD)  
-        scr.grid(column=0, row=2, sticky='WE', columnspan=3) 
+        scr.grid(column=0, row=2, padx=6,pady=5,sticky='WE', columnspan=3) 
     
         windows.mainloop()
     
@@ -103,13 +82,30 @@ class CRACKER(object):
     def msg_datecheck(self):
         mBox.showwarning("title","text")
         
-    def select_excel(self):
-        #filename=asksaveasfilename(parent = windows,defaultextension='.xls')
-        filename = askopenfilename(filetypes=[("excel格式",".xls")])
-        vartext.set(filename)
-        output = check_xls(filename)
-        scr.insert(tk.END, output)
+    def main_cracker(self):
+        global tag
+        tag = True
+        server = '127.0.0.1'
+        num_thread = 10
+        time_start = time.time()
+        pwlist = get_pwlist()
+        step = len(pwlist)//num_thread+1  
+        for n in range(num_thread):
+            t1 = Thread(target = cracker,args = (server,pwlist[step*n:step*n+step],str(n+1),))
+            t1.start()
+        t1.join()
     
+        if not tag:
+            print(u'密码字典测试完毕，未找到匹配密码，将遍历测试8位以内所有数字密码！')
+            for m in range(10):
+                t2 = Thread(target = cracker,args = (server,range(m*10000000,(m+1)*10000000),str(m+1)+'+',))
+                t2.start()
+            t2.join()
+            time_end = time.time()
+            time_use = time_end - time_start
+            print(u'用时 %d时%d分%d秒' % (time_use//3600,time_use%3600//60,time_use%60))
+        
+
 
     
     
